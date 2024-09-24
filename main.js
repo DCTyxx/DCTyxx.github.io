bf65de529009477c98bb1b71ceae90daecd91e3a
StartsYu <dctyxx@foxmail.com>
2024-09-25 00:17:17 +0800

    fix_bug

--- main.js
+++ main.js
@@ -709,10 +709,8 @@ async function main() {
 	const url = new URL(
 		// "nike.splat",
 		// location.href,
-		 params.get("url") || "train.splat",
-		//params.get("url") || "fox.splat",
-		//"https://huggingface.co/iamrajee/my-gsplat-data/resolve/main/",
-		 "https://huggingface.co/cakewalk/splat-data/resolve/main/",
+		params.get("url") || "statue.splat",
+		"https://huggingface.co/MiXiu/splat-data/resolve/main/",
 	);
 	const req = await fetch(url, {
 		mode: "cors", // no-cors, *cors, same-origin
